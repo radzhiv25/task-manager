8066740650a7b65d6e21bfe8954856208cb85d0f
radzhiv <radzhivkrishna@gmail.com>
2024-06-27 08:20:17 +0530

    feat. UI changes in TailwindConfig and added react-router

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        backgroundSize: {
+          '300%': '300%',
+        },
+        animation: {
+          gradient: 'animatedgradient 6s ease infinite alternate',
+        },
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
