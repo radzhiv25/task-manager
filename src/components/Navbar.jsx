3b6c743db1ea2414a3df495fca7fa2c6ba804950
radzhiv <radzhivkrishna@gmail.com>
2024-06-27 08:16:47 +0530

    feat. Navbar Updated with link and UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { BsGithub } from 'react-icons/bs'
+import React from "react";
+import { BsGithub } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div className='p-3 border-b mx-5 flex justify-between items-center'>
-        <div className="flex flex-col">
-            <h1 className='text-2xl text-gray-500 font-semibold leading-tight'>Task Manager</h1>
-            <p className='text-xs'>manage your tasks in few clicks</p>
-        </div>
-        <BsGithub className='size-8 hover:text-gray-500'/>
+    <div className="py-2 px-3 border rounded-full flex justify-between items-center bg-white">
+      <div className="flex items-center gap-2">
+        <span className="size-8 bg-gradient-to-br from-blue-500 to-green-400 animate-gradient bg-300% rounded-full"></span>
+        <span className="flex flex-col leading-tight">
+          <h1 className="md:text-2xl text-lg text-gray-500 font-semibold bg-gradient-to-tr from-blue-500 to-green-500 bg-clip-text text-transparent">
+            Task Manager
+          </h1>
+          <p className="text-xs">manage your tasks in few clicks</p>
+        </span>
+      </div>
+      <BsGithub className="size-8 hover:text-gray-500" />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
